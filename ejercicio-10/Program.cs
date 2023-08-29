5e9ff21e9d855726286bab1d10bfa117c389d30d
Sandra Carolina <Sandra Carolina@DESKTOP-SFK7P55>
2023-08-28 22:00:56 -0500

    Agregar archivos de proyecto.

---
+++ ejercicio-10/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float consumo;
+            double descuento;
+            double impuesto;
+            double subtotal;
+            double importe;
+
+            Console.WriteLine("Ingrese el consumo:");
+            consumo = Convert.ToSingle(Console.ReadLine());
+
+            if (consumo < 100)
+            {
+                descuento = consumo * 0.1;
+            }
+            else
+            {
+                descuento = consumo * 0.2;
+            }
+
+            impuesto = consumo * 0.18;
+
+            subtotal = consumo - descuento;
+
+            importe = subtotal + impuesto;
+
+            Console.WriteLine("Descuento: " + descuento);
+            Console.WriteLine("Impuesto: " + impuesto);
+            Console.WriteLine("Subtotal: " + subtotal);
+            Console.WriteLine("Importe: " + importe);
+            Console.ReadKey();
+        }
+    }
+}
